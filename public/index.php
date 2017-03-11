083ff28b4c46de57196bdd607c24cb4eaef5f9a8
Andrew Forward <aforward@gmail.com>
2017-03-11 08:13:19 -0500

    Initial commit with a base API.php and INDEX.php app

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>LAB 08</title>
+    <link type="text/css" rel="stylesheet" href="assets/styles.css">
+    <script type="text/javascript" src="assets/library.js"></script>
+  </head>
+  <body>
+    <p id="version">Loading ...</p>
+    <script type="text/javascript" src="assets/events.js"></script>
+  </body>
+</html>
